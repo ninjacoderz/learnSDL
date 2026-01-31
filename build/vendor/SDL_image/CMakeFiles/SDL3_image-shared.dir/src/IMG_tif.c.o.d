7e2c55d3b2628957c619a1efac8f5fbd43897266
Binh Nguyen Thanh <binhnguyenthanh@Binhs-MacBook-Pro-2.local>
2026-01-31 18:24:24 +0700

    Ignore build directory

--- build/vendor/SDL_image/CMakeFiles/SDL3_image-shared.dir/src/IMG_tif.c.o.d
+++
@@ -1,2 +0,0 @@
-vendor/SDL_image/CMakeFiles/SDL3_image-shared.dir/src/IMG_tif.c.o: \
-  /Users/binhnguyenthanh/Documents/GameProgramming/SDL3/FarmGame/vendor/SDL_image/src/IMG_tif.c
--- build/vendor/SDL_ttf/external/plutovg-build/CMakeFiles/plutovg.dir/source/plutovg-ft-math.c.o.d
+++
@@ -1,4 +0,0 @@
-vendor/SDL_ttf/external/plutovg-build/CMakeFiles/plutovg.dir/source/plutovg-ft-math.c.o: \
-  /Users/binhnguyenthanh/Documents/GameProgramming/SDL3/FarmGame/vendor/SDL_ttf/external/plutovg/source/plutovg-ft-math.c \
-  /Users/binhnguyenthanh/Documents/GameProgramming/SDL3/FarmGame/vendor/SDL_ttf/external/plutovg/source/plutovg-ft-math.h \
-  /Users/binhnguyenthanh/Documents/GameProgramming/SDL3/FarmGame/vendor/SDL_ttf/external/plutovg/source/plutovg-ft-types.h
